32959674aae4eb6c76bfc9e0573682ff9b144737
Lucca Augusto <51342214+LuccaAug@users.noreply.github.com>
2022-01-09 12:02:13 -0300

    Rota POST para o lançamento
    Criação do backend para envio de um lançamento

--- drpig/views.py
+++ drpig/views.py
@@ -1,11 +1,21 @@
-from rest_framework import mixins, generics, permissions
+from rest_framework import mixins, generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Lancamento, Categoria, MetodoDePagamento as Pagamento
 from .serializers import LancamentoSerializer, CategoriaSerializer, PagamentoSerializer
 
-class LancamentoListAPIView(generics.ListAPIView):
-    serializer_class = LancamentoSerializer
-    permission_classes = [permissions.AllowAny]
-    queryset = Lancamento.objects.all()
+class LancamentosAPI(APIView):
+    def get(self, request):
+        queryset = Lancamento.objects.all()
+        serializer = LancamentoSerializer(queryset, many=True)
+        print(str(dict(serializer.data[0])).replace("'", '"'))
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = LancamentoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
 class CategoriaListAPIView(generics.ListAPIView):
     serializer_class = CategoriaSerializer
