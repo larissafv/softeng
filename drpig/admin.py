46bcfe0bc78bf20383fbabdf95ea3622c25b18c7
Daniel Victor <vicdaniel.ferreira@gmail.com>
2022-01-08 23:55:44 -0300

    Adicionar tabelas novas em admin

--- drpig/admin.py
+++ drpig/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Lancamento
+from .models import Lancamento, Categoria, MetodoDePagamento
 
 admin.site.register(Lancamento)
+admin.site.register(Categoria)
+admin.site.register(MetodoDePagamento)
